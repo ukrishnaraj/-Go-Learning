02f26914772828762775259ceab4edfa99a0dbfa
ukrishnaraj <krishnaraj.udayakumar@gmail.com>
2017-01-10 14:32:09 +0530

    tsp0.1

--- tsp/tsp.go
+++ tsp/tsp.go
@@ -1,35 +1,57 @@
-/*package main
+package main
 
 import "fmt"
 
+var cost int
 func main()  {
-    var n int
+    var n,start int
     fmt.Println("Enter number of nodes:")
-    fmt.Scanf("%d",&n)
-    matrix := make([][]uint8, n) 
+    fmt.Scanf("%d\n",&n)
+    matrix := make([][]int, n) 
     for i,_ := range matrix {
-        matrix[i] = make([]uint8, n+1)
+        matrix[i] = make([]int, n)
         fmt.Println("Enter elements of row ",i)
         for k,_ := range matrix[i]{
-            fmt.Scanf("%d",&matrix[i][k])
+            fmt.Scanf("%d\n",&matrix[i][k])
         }
     }
-    fmt.Println(matrix)
+    fmt.Println("Enter the starting node :")
+    fmt.Scanf("%d\n",&start)
+    fmt.Println(tsp(matrix,start))
 }
 
-func tsp(a []int) int  {
-    var minimum = 999 
-    var next int
-    for i := 1; i < len(a); i++ {
-        
+func tsp(a [][]int,start int) []int  {
+    travel := make([]int, len(a)+1)
+    visited := make([]int, len(a))
+    for i := range visited{
+        visited[i]=0
     }
+    for i:=0; i< len(a)-1; i++{
+        travel[i] = start
+        if i==len(a)-1 {
+            travel[i+1]= travel[0]
+        }
+        visited[start] = 1 
+        new := nextNode(a[start],visited,start)
+        if new !=0{
+            start = new
+        } else {
+            start = travel[0]
+        }
+    }
+    return travel
+}
+
+func nextNode(a,visited []int,x int) int  {
+    var next int
+    min := 999
+    for i := 0; i< len(a); i++{ 
+        
+        if a[i] != 0 && visited[i]==0 && a[i]<min{
+                min = a[i]
+                next = i
+        }
+    }
+    fmt.Println(".$$52##",min)
     return next
-}
-
-func next(a [][]uint8) int  {
-    var next int
-    var visited int
-    for i := 1; i<= len(a)-1; i++{
-        
-    }
 }
